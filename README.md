58fff1120863b33d16a1a18073fd184ec4e0c571
thzincxss <79030984+thzincxss@users.noreply.github.com>
2021-12-11 22:52:24 +0000

    interfacezinha hum hum

--- README.md
+++ README.md
@@ -4,29 +4,25 @@
 <h3>💻 Termux</h3>
 <h3>$ pkg upgrade && pkg update -y</h3>
 <h3>$ pkg install git python -y</h3>
-<h3>$ pip install requests </h3>
 <h3>$ git clone https://github.com/thzincxss/consultaCEP</h3>
 <h3>$ cd consultaCEP</h3>
 <h3>$ python3 bot.py</h3>
 
 <h3>💻 Debian</h3>
 <h3>$ apt upgrade && apt update -y</h3>
-<h3>$ apt install git python -y</h3>
-<h3>$ pip install requests </h3>
+<h3>$ apt install git python -y</h3
 <h3>$ git clone https://github.com/thzincxss/consultaCEP</h3>
 <h3>$ cd consultaCEP</h3>
 <h3>$ python3 bot.py</h3>
 
 <h3>💻 Arch Linux</h3>
 <h3>$ pacman -Syyu git python -y</h3>
-<h3>$ pip install requests </h3>
 <h3>$ git clone https://github.com/thzincxss/consultaCEP</h3>
 <h3>$ cd consultaCEP</h3>
 <h3>$ python3 bot.py</h3>
 
 <h3>💻 OpenSUSE</h3>
 <h3>$ zypper install python git -y</h3>
-<h3>$ pip install requests </h3>
 <h3>$ git clone https://github.com/thzincxss/consultaCEP</h3>
 <h3>$ cd consultaCEP</h3>
 <h3>$ python3 bot.py</h3>
--- bot.py
+++ bot.py
@@ -1,7 +1,27 @@
 import json
 import requests
+import os
+import time
 
+os.system('clear') or None
+os.system('pip install requests') or None
+os.system('pip install json') or None
+os.system('clear') or None
+
+txt = """
+╔════╦╗░╔╗░░░╔═══╗░░░░░░░░░░ ╔╗╔╗
+║╔╗╔╗║║░║║░░░║╔═╗║░░░░░░░░░░░║╠╝╚╗
+╚╝║║╚╣╚═╝╠═══╣║░╚╬══╦═╗╔══╦╗╔╣╠╗╔╬══╗
+░░║║░║╔═╗╠══║║║░╔╣╔╗║╔╗╣══╣║║║║║║║╔╗║
+░░║║░║║░║║║══╣╚═╝║╚╝║║║╠══║╚╝║╚╣╚╣╔╗║
+░░╚╝ ╚╝░╚╩═══╩═══╩══╩╝╚╩══╩══╩═╩═╩╝╚╝
+
+"""
+print(txt)
 pesquisa_cep = input("digite um cep:")
+
+os.system('clear') or None
+
 dados = requests.get("https://cep.awesomeapi.com.br/json/{}".format(pesquisa_cep))
 dados = dados.json()
 cep = dados['cep']
@@ -14,3 +34,18 @@ cidade = dados['city']
 ibge = dados['city_ibge']
 ddd = dados['ddd']
 print("CEP:{}\nRua:{}\nEstado:{}\nBairro:{}\nLatitude:{}\nLongitude:{}\nCidade:{}\nIBGE:{}\nDDD:{}".format(cep, rua, estado, bairro, latitude, longitude, cidade, ibge, ddd))
+
+inpu = input("\nRealizar nova consulta? [s][n]\n")
+if inpu == 's':
+  os.system('python bot.py') or None
+elif inpu == 'n':
+  varad = "Adeus em 3.."
+  print(varad)
+  time.sleep(1)
+  varad = "Adeus em 32."
+  print(varad)
+  time.sleep(1)
+  varad = "Adeus em 321"
+  print(varad, 'fechando..')
+  time.sleep(1)
+  os.system('clear') or None
